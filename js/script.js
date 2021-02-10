6aad990987911c320ae2a7614b795653395cc2e5
edgargc026 <edgargc_26@hotmail.com>
2021-02-10 01:07:56 -0600

    Se han comentado para que sirve cada metodo y se ha implementado un metodo para que se pueda ordernar los numeros del arreglo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 class Game {
   constructor() {}
 
+  //Creamos un metodo donde se generen los numeros aleatoreamente
   getRandomNumberPokemon(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -8,13 +9,24 @@ class Game {
     return Math.floor(Math.random() * (1 + max - min)) + min;
   }
 
+  //Este método se encarga de ordernar los numeros de menor a mayor
+  orderNumberElements(array) {
+    return array.sort(
+      (firstNumber, secondNumber) => firstNumber - secondNumber
+    );
+  }
+
+  //Este metodo llama la funcion para generar los numeros de manera aleatoria
+  //Estos numeros seran guardados en un arreglo ordenado donde se verificaran
+  //que no esten repetidos. En caso de estar repetidos, sera sustituido por otro
+  //numero.
   getRandomPokemon() {
     let array = [];
 
     for (let i = 0; i < 10; i++) {
       array.push(this.getRandomNumberPokemon(1, 20));
     }
-    array.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
+    this.orderNumberElements(array);
     console.log(array);
   }
 }
